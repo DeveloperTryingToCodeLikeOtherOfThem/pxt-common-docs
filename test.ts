ba8c4004d74ae32581b2ee776aa77165a3d60087
a012 <Aeagan0507@gmail.com>
2026-04-04 18:15:37 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
+// for testing if these functions make sense and should appear in this repo
+control._onCodeStart(0);
+control._onCodeStop(0);
 
+control.programName();
+control.programHash();
+
+const buf = control.createBuffer(100);
