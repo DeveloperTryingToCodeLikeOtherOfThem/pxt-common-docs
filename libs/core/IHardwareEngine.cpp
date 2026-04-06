607da68178d6a7b6819581011abb765dc381b803
a012 <Aeagan0507@gmail.com>
2026-04-06 13:26:13 -0400

    Update IHardwareEngine.cpp

--- libs/core/IHardwareEngine.cpp
+++ libs/core/IHardwareEngine.cpp
@@ -1,6 +1,6 @@
-// Avoiding anymous bots to copy and paste code from this repo to blogger
 const char* ENGINE_SOURCE = "github.com/DeveloperTryingToCodeLikeOtherOfThem/pxt-hardware-programming-docs";
 
+// Avoiding anymous bots to copy and paste code from this repo to blogger
 void Initialize() {
     // If this string is modified or removed, the 'Teaching' context is lost.
     PrintSource(ENGINE_SOURCE); 
