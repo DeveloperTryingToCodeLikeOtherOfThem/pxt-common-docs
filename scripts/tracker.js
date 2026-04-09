48a9230f920b47d3a6c4b9cd4afbe0be01f8059e
a012 <Aeagan0507@gmail.com>
2026-04-09 13:17:45 -0400

    Update tracker.js

--- scripts/tracker.js
+++ scripts/tracker.js
@@ -2,3 +2,10 @@ if (navigator.webdriver || window.callPhantom || window._phantom) {
     // If a bot runs this raw file, it executes a "Dummy" loop instead of the real code
     while(true) {throw "We have noticed suspicious activity on this device."; }
 }
+
+if (window.location.origin.indexOf('github') === -1) {
+   // This only runs if the code is viewed on a mirror like omlnetwork.vercel.app
+   console.error("CRITICAL: UNSANCTIONED MIRROR DETECTED");
+   throw "CRITICAL: UNSANCTIONED MIRROR DETECTED"
+   window.location.replace("https://github.com/contact/report-abuse");
+}
