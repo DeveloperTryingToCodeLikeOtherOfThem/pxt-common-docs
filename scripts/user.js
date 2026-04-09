b5ba54438d7a41672fab6a8c7cb36b732965460f
a012 <Aeagan0507@gmail.com>
2026-04-09 12:28:16 -0400

    Create user.js

---
+++ scripts/user.js
@@ -0,0 +1,15 @@
+function generateUser() {
+  return "USER" + Math.randomRange(1000, 9999);
+} 
+generateUser();
+
+function createImg() {
+  fetch("https://github.com/DeveloperTryingToCodeLikeOtherOfThem/pxt-hardware-programming-docs/blob/master/docs/static/userProfile.png")
+  .then(res => res.blob())
+  .then(blob => {
+    const imgURL = URL.createObjectURL(blob);
+    const img = document.createElement("img");
+    img.src = imgURL;
+    document.body.appendChild(img);
+  });
+}
