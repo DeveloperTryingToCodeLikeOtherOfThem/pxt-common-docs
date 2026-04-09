76435f95de68e2e1369864f38df54bda92801a02
a012 <Aeagan0507@gmail.com>
2026-04-09 12:28:57 -0400

    Forgot that I already created a profile method in the first place.

--- scripts/user.js
+++ scripts/user.js
@@ -1,9 +1,10 @@
 function generateUser() {
+  createProfile();
   return "USER" + Math.randomRange(1000, 9999);
 } 
 generateUser();
 
-function createImg() {
+function createProfile() {
   fetch("https://github.com/DeveloperTryingToCodeLikeOtherOfThem/pxt-hardware-programming-docs/blob/master/docs/static/userProfile.png")
   .then(res => res.blob())
   .then(blob => {
