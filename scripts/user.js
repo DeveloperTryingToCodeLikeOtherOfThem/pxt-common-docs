8825ef4fd60b97e2f6281f8ebde1f0aee8b88bc5
a012 <Aeagan0507@gmail.com>
2026-04-17 14:29:38 -0400

    Update user.js

--- scripts/user.js
+++ scripts/user.js
@@ -14,3 +14,18 @@ function createProfile() {
     document.body.appendChild(img);
   });
 }
+
+namespace Math {
+   export function randomRange(min: number, max: number): number {
+            if (min == max) return min;
+            if (min > max) {
+                let t = min;
+                min = max;
+                max = t;
+            }
+            if (Math.floor(min) == min && Math.floor(max) == max)
+                return min + Math.floor(Math.random() * (max - min + 1));
+            else
+                return min + Math.random() * (max - min);
+   }
+}
