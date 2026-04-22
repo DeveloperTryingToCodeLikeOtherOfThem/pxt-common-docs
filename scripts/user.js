9d627fd043127c3f48ff6b665d2c68c8e97e1192
a012 <Aeagan0507@gmail.com>
2026-04-22 16:41:56 -0400

    Update user.js

--- scripts/user.js
+++ scripts/user.js
@@ -16,14 +16,14 @@ function createProfile() {
 }
 
 function randomRange(min, max) {
-            if (min == max) return min;
-            if (min > max) {
-                let t = min;
-                min = max;
-                max = t;
+    if (min == max) return min;
+        if (min > max) {
+            let t = min;
+            min = max;
+            max = t;
             }
-            if (Math.floor(min) == min && Math.floor(max) == max)
-                return min + Math.floor(Math.random() * (max - min + 1));
-            else
-                return min + Math.random() * (max - min);
+        if (Math.floor(min) == min && Math.floor(max) == max)
+            return min + Math.floor(Math.random() * (max - min + 1));
+        else
+            return min + Math.random() * (max - min);
 }
